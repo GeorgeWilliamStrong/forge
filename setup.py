80d62f1c638fef628907a76a7feed798b65b58d8
georgewilliamstrong <geowstrong@gmail.com>
2022-08-30 14:09:41 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='forge',
+    version='1.0',
+    description = 'A full-waveform inversion framework built in PyTorch',
+    long_description = 'A full-waveform inversion framework built in PyTorch', 
+    author='George Strong',
+    author_email='geowstrong@gmail.com',
+    license='AGPL-3.0',
+    python_requires=">=3.9",
+    packages = find_packages(),
+    install_requires=required)
