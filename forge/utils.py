74d4cf06d0666f6b69440ff960e50eb057d12dcf
George Strong <gs914@ic.ac.uk>
2024-04-26 14:45:51 +0100

    finish docs for utils.py

--- forge/utils.py
+++ forge/utils.py
@@ -126,16 +126,17 @@ def resample(model, old_spacing, new_spacing, **kwargs):
         Old grid spacing.
     new_spacing : tuple of float
         New grid spacing.
-    order : int, optional
-        Order of the interpolation. Defaults to 3.
-    prefilter : bool, optional
-        Whether prefiltering needs to be applied before interpolation.
-        If downsampling, this defaults to False as an anti-aliasing filter
-        will be applied instead. If upsampling, this defaults to True.
-    anti_alias : bool, optional
-        Whether a Gaussian filter is applied to smooth the model before
-        interpolation. Defaults to True. This is only applied when
-        downsampling.
+    **kwargs : additional keyword arguments
+        order : int, optional
+            Order of the interpolation. Defaults to 3.
+        prefilter : bool, optional
+            Whether prefiltering needs to be applied before interpolation.
+            If downsampling, this defaults to False as an anti-aliasing filter
+            will be applied instead. If upsampling, this defaults to True.
+        anti_alias : bool, optional
+            Whether a Gaussian filter is applied to smooth the model before
+            interpolation. Defaults to True. This is only applied when
+            downsampling.
 
     Returns
     -------
@@ -166,25 +167,78 @@ def resample(model, old_spacing, new_spacing, **kwargs):
     return interp
 
 
-def trace_plot(data, shot, scale_fac=1, size=(8,4), line_color='black', linewidth=1, fill=True, fill_color='black', fill_color2=None, fill_fac=1e-2, save=None):
-    time = np.arange(0, data.shape[2], 1)
+def trace_plot(data, shot, **kwargs):
+    """
+    Plot seismic traces from shot gathers.
+
+    Parameters
+    ----------
+    data : array-like
+        3D array containing acoustic data. Dimensions should be
+        [num shots, num traces/receivers per shot, num time steps].
+    shot : int
+        Index of the shot to plot.
+    **kwargs : additional keyword arguments
+        scale_fac : float, optional
+            Scaling factor for trace amplitudes. Default is 1.
+        size : tuple, optional
+            Size of the plot figure. Default is (8, 4).
+        line_color : str, optional
+            Color of the trace lines. Default is 'black'.
+        linewidth : float, optional
+            Width of the trace lines. Default is 1.
+        fill : bool, optional
+            Whether to fill between traces. Default is True.
+        fill_color : str, optional
+            Color for filling between traces. Default is 'black'.
+        fill_color2 : str, optional
+            Second color for filling between traces if alternating colors are
+            desired. Default is None.
+        fill_fac : float, optional
+            Fill factor threshold. Default is 1e-2.
+        save : str or None, optional
+            File path to save the plot as an image. Default is None.
+
+    Returns
+    -------
+
+    """
+    scale_fac = kwargs.pop('scale_fac', 1)
+    size = kwargs.pop('size', (8, 4))
+    line_color = kwargs.pop('line_color', 'black')
+    linewidth = kwargs.pop('linewidth', 1)
+    fill = kwargs.pop('fill', True)
+    fill_color = kwargs.pop('fill_color', 'black')
+    fill_color2 = kwargs.pop('fill_color2', None)
+    fill_fac = kwargs.pop('fill_fac', 1e-2)
+    save = kwargs.pop('save', None)
+
+    num = data.shape[2]
+    time = np.arange(0, num, 1)
     fig, ax1 = plt.subplots(figsize=size)
     for i in range(len(data[shot])):
-        ax1.plot((scale_fac * data[shot][i]) + i, time, color=line_color, linewidth=linewidth)
+        ax1.plot((scale_fac * data[shot][i]) + i, time, color=line_color,
+                 linewidth=linewidth)
         if fill:
             if fill_color2:
-                if i%2 == 0:
+                if i % 2 == 0:
                     ax1.fill_betweenx(time, i, (scale_fac * data[shot][i]) + i,
-                                 where=(((scale_fac * data[shot][i]) + i) > i + fill_fac), color=fill_color)
+                                      where=(((scale_fac * data[shot][i]) + i)
+                                             > i + fill_fac),
+                                      color=fill_color)
                 else:
                     ax1.fill_betweenx(time, i, (scale_fac * data[shot][i]) + i,
-                                 where=(((scale_fac * data[shot][i]) + i) > i + fill_fac), color=fill_color2)
+                                      where=(((scale_fac * data[shot][i]) + i)
+                                             > i + fill_fac),
+                                      color=fill_color2)
             else:
                 ax1.fill_betweenx(time, i, (scale_fac * data[shot][i]) + i,
-                                 where=(((scale_fac * data[shot][i]) + i) > i + fill_fac), color=fill_color)
+                                  where=(((scale_fac * data[shot][i]) + i)
+                                         > i + fill_fac),
+                                  color=fill_color)
     ax1.invert_yaxis()
     plt.xlim((-1, data.shape[1]))
-    plt.ylim((data.shape[2]-1, 0))
+    plt.ylim((num-1, 0))
     plt.xlabel('trace number')
     plt.ylabel('time-step')
     if save:
@@ -192,9 +246,33 @@ def trace_plot(data, shot, scale_fac=1, size=(8,4), line_color='black', linewidt
     plt.show()
 
 
-def trace_normalize(data, dim=-1):
+def normalize(data, type='trace'):
+    """
+    Normalize acoustic data along specified dimensions.
+
+    Parameters
+    ----------
+    data : torch.Tensor
+        Seismic data tensor.
+    type : str or tuple or None, optional
+        Type of normalization. If 'trace', normalize along the last dimension.
+        If 'shot', normalize along the last two dimensions. If a tuple,
+        normalize along the specified dimensions. Default is None.
+
+    Returns
+    -------
+    torch.Tensor
+        Normalized acoustic data tensor.
+
+    """
+    if type == 'trace':
+        dim = -1
+    elif type == 'shot':
+        dim = (-2, -1)
+    elif isinstance(type, tuple):
+        dim = type
+    else:
+        raise ValueError("Invalid value for 'type'. It must be 'trace', \
+'shot', or a tuple.")
+
     return data/torch.norm(data, dim=dim, keepdim=True)
-
-
-def shot_normalize(data):
-    return data/torch.norm(data, dim=(-2, -1), keepdim=True)
