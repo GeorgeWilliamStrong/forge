9493a45d909ddd08d5fb97561947b67e94ba72b1
George Strong <42313440+GeorgeWilliamStrong@users.noreply.github.com>
2024-04-27 22:23:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open('requirements.txt') as f:
 setup(
     name='forge',
     version='1.0',
-    description = 'A full-waveform inversion framework built in PyTorch',
-    long_description = 'A full-waveform inversion framework built in PyTorch', 
+    description='A full-waveform inversion framework built in PyTorch',
+    long_description='A full-waveform inversion framework built in PyTorch', 
     author='George Strong',
     author_email='geowstrong@gmail.com',
     license='AGPL-3.0',
     python_requires=">=3.9",
-    packages = find_packages(),
+    packages=find_packages(),
     install_requires=required)
